dc5130ada078a8d0e30f880e32f2fe3cbfd7a24a
Nelio Alves <acenelio@gmail.com>
2018-10-01 16:52:33 -0300

    Correcao no uri1021

--- src/uri1021.java
+++ src/uri1021.java
@@ -16,8 +16,14 @@ public class uri1021 {
 		
 		// Vamos multiplicar N por 100 e forçar a conversão para int.
 		// Desse modo, por exemplo, 576.73 vai se tornar 57673
-		
-		resto = (int) (N * 100);
+
+		// Vamos tambem somar 0.5 antes de converter, para assegurar 
+		// que o numero seja devidamente arredondado, pois o tipo double
+		// as vezes da problema de arredondamento (por exemplo: se digitarmos
+		// 576.81 e multiplicarmos por 100, o resultado sera 57680.99999999,
+		// dai o casting resultaria em 57680 e nao 57681 como desejado)
+
+		resto = (int) (N * 100.0 + 0.5);
 
 		System.out.println("NOTAS:");
 
